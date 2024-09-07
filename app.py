3a04926d83993d59814798744f62dd8bef0e4871
Anushya Senthilkumar <anushyamaheshwaran@gmail.com>
2024-09-07 16:03:20 +0800

    updated

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     #print("Starting app on port %d" % port)
     #app.run(debug=True)
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='127.0.0.1',port=8080)
